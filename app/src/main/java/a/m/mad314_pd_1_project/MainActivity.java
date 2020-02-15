73ec8bb32702612c19772ef817bee715d6299224
harshilr <hramswaroop@gmail.com>
2020-02-14 21:46:13 -0500

    Initial Commit

---
+++ app/src/main/java/a/m/mad314_pd_1_project/MainActivity.java
@@ -0,0 +1,14 @@
+package a.m.mad314_pd_1_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
