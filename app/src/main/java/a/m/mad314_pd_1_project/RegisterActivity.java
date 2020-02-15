6a6c3d66001345cb52524af53c5bd1fbd0e14e12
harshilr <hramswaroop@gmail.com>
2020-02-15 09:33:22 -0500

    Login activity added

---
+++ app/src/main/java/a/m/mad314_pd_1_project/RegisterActivity.java
@@ -0,0 +1,15 @@
+package a.m.mad314_pd_1_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+}
