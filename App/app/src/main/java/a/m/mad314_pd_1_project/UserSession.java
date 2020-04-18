d411b6bba96b72c385ec15fdd8f5bc528d96ae90
Urvil <urvilshah56@gmail.com>
2020-04-17 20:11:28 -0400

    Added Retrofit Client

---
+++ App/app/src/main/java/a/m/mad314_pd_1_project/UserSession.java
@@ -0,0 +1,58 @@
+package a.m.mad314_pd_1_project;
+
+import android.content.SharedPreferences;
+
+class UserSession {
+
+    private UserSession(){}
+    private static SharedPreferences userPreferences;
+    private static UserSession userSession;
+
+    public static UserSession getInstance(SharedPreferences preferences){
+        if(userPreferences == null){
+            userPreferences = preferences;
+        }
+
+        if(userSession == null)
+            userSession = new UserSession();
+
+        return userSession;
+    }
+
+    public static UserSession getInstance(){
+        if(userSession == null)
+            userSession = new UserSession();
+
+        return userSession;
+    }
+
+    public  void clearSession(){
+        userPreferences.edit().putString("token", "").commit();
+        userPreferences.edit().putString("userId", "").commit();
+        userPreferences.edit().putString("email", "").commit();
+    }
+
+    public void setUserId(String userId){
+        userPreferences.edit().putString("userId", userId).commit();
+    }
+
+    public void setEmail(String email){
+        userPreferences.edit().putString("email", email).commit();
+    }
+
+    public void setToken(String token){
+        userPreferences.edit().putString("token", token).commit();
+    }
+
+    public String getUseId(){
+        return userPreferences.getString("userId", "");
+    }
+
+    public String getEmail(){
+        return userPreferences.getString("email", "");
+    }
+
+    public String getToken(){
+        return userPreferences.getString("token", "");
+    }
+}
---
+++ App/app/src/main/java/a/m/mad314_pd_1_project/responsemodel/LoginResponseModel.java
@@ -0,0 +1,31 @@
+package a.m.mad314_pd_1_project.responsemodel;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class LoginResponseModel {
+
+    @SerializedName("token")
+    @Expose
+    private String token;
+
+    @SerializedName("userid")
+    @Expose
+    private String userid;
+
+    public String getUserId() {
+        return userid;
+    }
+
+    public void setUserId(String userId) {
+        userid = userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
